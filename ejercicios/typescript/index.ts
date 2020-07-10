68714d86e398b512ae4da819ab1e6a946e58cf00
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-07-09 23:43:05 -0600

    ok

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -5,3 +5,41 @@ function add(a: number, b: number): number {
 }
 
 const sum = add(5, 10);
+
+// Boolean
+let muted: boolean = true;
+muted = false;
+
+// Numeros
+let numerador : number = 42;
+
+// String
+let nombre : string = 'Luis';
+
+// Arreglos
+let people: string[] = [];
+people = ['Luis','Reyna'];
+people.push('9000');
+
+let peopleAndNumbers: Array<string | number | boolean> = [];
+peopleAndNumbers.push('Luis');
+peopleAndNumbers.push(true);
+peopleAndNumbers.push(3000);
+
+enum Color {
+  Rojo,
+  Verde,
+  Azul
+}
+
+enum Color2 {
+  Rojo = 'Rojo',
+  Verde = 'Verde',
+  Azul = 'Azul'
+}
+
+let colorFavorito:Color = Color.Rojo;
+console.log(`mi color favorito es ${colorFavorito}`);
+
+let colorFavorito2:Color2 = Color2.Rojo;
+console.log(`mi color favorito es ${colorFavorito2}`);
